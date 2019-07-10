403631a889c2caac82bb74c1a3bf0c86e41f3db0
Raystern <raysternwong@gmail.com>
2019-07-10 09:32:45 +0800

    Add project files.

---
+++ GoogleDevGuide/PuzzleSolver.cs
@@ -0,0 +1,15 @@
+﻿using GoogleDevGuide.Interface;
+using System.Collections.Generic;
+
+namespace GoogleDevGuide
+{
+    class PuzzleSolver : IPuzzleSolver
+    {
+        public string FindLongestWordInSubsequence(string target, List<string> words)
+        {
+            string result = string.Empty;
+
+            return result;
+        }
+    }
+}
