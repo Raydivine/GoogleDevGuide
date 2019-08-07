d3a4d479a6ad1eb5f85ee853aa081b125d12ed7f
RaysternWong <raysternwong@gmail.com>
2019-08-07 17:26:59 +0800

    Implemented some sub methods and unit tests

--- GoogleDevGuide/PuzzleSolver.cs
+++ GoogleDevGuide/PuzzleSolver.cs
@@ -1,4 +1,6 @@
 ﻿using GoogleDevGuide.Interface;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GoogleDevGuide
 {
@@ -11,7 +13,80 @@ namespace GoogleDevGuide
         /// <returns></returns>
         public double LakeVolumeCalculate(double[] groundHeights)
         {
-            return 0;
+            double sum = 0, lakeSize = 0, startPeak = 0, endPeak = 0;
+            List<double> grounds = new List<double>();
+
+
+            if(groundHeights == null || groundHeights.Length < 3)
+            {
+                return sum;
+            }
+
+            for (int i = 0; i < groundHeights.Length; i++)
+            {
+                startPeak = groundHeights[i];
+
+                if (groundHeights[i+1] >= startPeak)
+                {
+                    continue;
+                }
+            }
+
+            return sum;
         }
+
+        private double GetSumOfVolumeBetweenTwoPeak(double[] groundHeights, ref int i)
+        {
+            if (IsThereVolume(groundHeights,i) == false)
+            {
+                return 0;
+            }
+
+            double lakeSize = 0, startPeak, current, endPeak;
+            List<double> floors = new List<double>();
+
+            startPeak = groundHeights[i];
+            ++i;
+            do
+            {
+                current = groundHeights[i];
+                floors.Add(current);
+                endPeak = groundHeights[++i];
+
+                if (groundHeights.Length - i == 1)
+                {
+                    break;
+                }
+
+            } while (endPeak < startPeak);
+
+            double shorterPeak = (startPeak <= endPeak) ? startPeak : endPeak;
+
+            foreach(var floor in floors)
+            {
+                lakeSize += (shorterPeak - floor);
+            }
+
+            return lakeSize;
+        }
+
+        private bool IsThereVolume(double[] groundHeights, int i)
+        {
+            if (groundHeights != null &&
+                groundHeights.Length - i >= 3 &&
+                groundHeights[i] > groundHeights[i+1])
+            {
+                var list = groundHeights.ToList();
+                list.RemoveRange(0, i + 2);
+     
+                if(list.Any(x => x > groundHeights[i+1]))
+                {
+                    return true;
+                }           
+            }
+
+            return false;
+        }
+
     }
 }
--- GoogleDevGuideTest/IPuzzleSolverTest.cs
+++ GoogleDevGuideTest/IPuzzleSolverTest.cs
@@ -1,14 +1,17 @@
 ﻿using GoogleDevGuide;
 using GoogleDevGuide.Interface;
+using System;
+using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace GoogleDevGuideTest
 {
-    public class IPuzzleSolverTest
+    public class LakeVolumeCalculateTest
     {
         IPuzzleSolver _IPuzzleSolver;
 
-        public IPuzzleSolverTest()
+        public LakeVolumeCalculateTest()
         {
             _IPuzzleSolver = new PuzzleSolver();
         }
@@ -20,7 +23,7 @@ namespace GoogleDevGuideTest
         public void LakeVolumeCalculate_emptyInput_Return0(double[] groundHeights)
         {
             double expected = 0;
-
+           
             double result = _IPuzzleSolver.LakeVolumeCalculate(groundHeights);
 
             Assert.Equal(expected, result);
@@ -119,8 +122,128 @@ namespace GoogleDevGuideTest
             Assert.Equal(expected, result);
         }
 
+        [Fact]
+        public void GetVolumeTillEndPeak_ThreeLakesCase1_ReturnResult()
+        {
+            double[] groundHeights = new double[15] { 1, 3, 2, 4, 1, 3, 1, 4, 5, 2, 2, 1, 4, 2, 2 };
+            double expected = 15;
 
+            double result = _IPuzzleSolver.LakeVolumeCalculate(groundHeights);
 
+            Assert.Equal(expected, result);
+        }
+    }
 
+    public class PuzzleSolverPrvtMthdTest
+    {
+        object _obj;
+        Type _type;
+
+        public PuzzleSolverPrvtMthdTest()
+        {
+            _type = typeof(PuzzleSolver);
+            _obj = Activator.CreateInstance(_type);
+        }
+
+        [Theory]
+        [InlineData(new double[0],0)]
+        [InlineData(new double[3]{ 0, 0, 0 },0)]
+        [InlineData(null,0)]
+        public void GetSumOfVolumeBetweenTwoPeak_InputIsBlank_Return0(double[] groundHeights, int i)
+        {
+            MethodInfo method = _type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(x => x.Name == "GetSumOfVolumeBetweenTwoPeak" && x.IsPrivate)
+            .First();
+
+            double expected = 0;
+
+            double result = (double)method.Invoke(_obj, new object[] { groundHeights, i });
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new double[3] { 3, 3, 3 }, 0)]
+        [InlineData(new double[3] { 3, 2, 2 }, 0)]
+        [InlineData(new double[3] { 3, 2, 1 }, 0)]
+        public void GetSumOfVolumeBetweenTwoPeak_NoPeaks_Return0(double[] groundHeights, int i)
+        {
+            MethodInfo method = _type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(x => x.Name == "GetSumOfVolumeBetweenTwoPeak" && x.IsPrivate)
+            .First();
+
+            double expected = 0;
+
+            double result = (double)method.Invoke(_obj, new object[] { groundHeights, i });
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetSumOfVolumeBetweenTwoPeak_OneValue_ReturnResult()
+        {
+            MethodInfo method = _type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(x => x.Name == "GetSumOfVolumeBetweenTwoPeak" && x.IsPrivate)
+            .First();
+
+            int i = 0;
+            double[] groundHeights = new double[3] { 3, 0, 3 };
+            double expected = 3;
+
+            double result = (double)method.Invoke(_obj, new object[] { groundHeights, i });
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetSumOfVolumeBetweenTwoPeak_OneValueCase2_ReturnResult()
+        {
+            MethodInfo method = _type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(x => x.Name == "GetSumOfVolumeBetweenTwoPeak" && x.IsPrivate)
+            .First();
+
+            int i = 0;
+            double[] groundHeights = new double[3] { 3, 2, 8 };
+            double expected = 1;
+
+            double result = (double)method.Invoke(_obj, new object[] { groundHeights, i });
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void GetSumOfVolumeBetweenTwoPeak_LakeSizeTwo_ReturnResult()
+        {
+            MethodInfo method = _type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(x => x.Name == "GetSumOfVolumeBetweenTwoPeak" && x.IsPrivate)
+            .First();
+
+            int i = 0;
+            double[] groundHeights = new double[4] { 3, 0, 1, 3 };
+            double expected = 3 + 2;
+
+            double result = (double)method.Invoke(_obj, new object[] { groundHeights, i });
+
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new double[0], 0)]
+        [InlineData(new double[3] { 0, 0, 0 }, 0)]
+        [InlineData(null, 0)]
+        [InlineData(new double[3] { 3, 3, 3 }, 0)]
+        [InlineData(new double[3] { 3, 2, 2 }, 0)]
+        [InlineData(new double[3] { 3, 2, 1 }, 0)]
+        [InlineData(new double[3] { 1, 2, 3 }, 0)]
+        public void IsThereVolume_NoPeaks_ReturnFalse(double[] groundHeights, int i)
+        {
+            MethodInfo method = _type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+            .Where(x => x.Name == "IsThereVolume" && x.IsPrivate)
+            .First();
+
+            bool result = (bool)method.Invoke(_obj, new object[] { groundHeights, i });
+
+            Assert.False(result);
+        }
     }
 }
